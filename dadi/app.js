0abbb68d88537eecf500658db6f4f62ea3ee1701
Bilobra <bra.carrozzo@gmail.com>
2022-07-25 14:56:06 +0200

    add ciclo for esercizio 2

--- dadi/app.js
+++ dadi/app.js
@@ -1,2 +1,18 @@
 // console.log prova
 console.log('prova')
+
+// creo una lista array da 1 a 6 
+const numeriDado = [1, 2, 3, 4, 5, 6]
+// creao una seconda e una terza lista array senza inserire i dati
+let numeriHuman = []
+let numeriBot = []
+
+// creo un ciclo che controlli i miei numeri
+
+for(let i=0 ; i < numeriDado.length; i++){
+    console.log(numeriDado[i])
+}
+
+
+
+
