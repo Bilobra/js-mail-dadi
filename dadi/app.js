c9c83a633c2bfa9603253e76ddc7b42bad75731b
Bilobra <bra.carrozzo@gmail.com>
2022-07-25 15:08:36 +0200

    add condizioni esercizio 2

--- dadi/app.js
+++ dadi/app.js
@@ -2,15 +2,30 @@
 console.log('prova')
 
 // creo una lista array da 1 a 6 
-const numeriDado = [1, 2, 3, 4, 5, 6]
-// creao una seconda e una terza lista array senza inserire i dati
-let numeriHuman = []
-let numeriBot = []
+let numeriDado = [1, 2, 3, 4, 5, 6]
 
-// creo un ciclo che controlli i miei numeri
+// creao due variabili random x generare numeri 
+let numeriHuman = Math.floor(Math.random()*numeriDado.length)+1
+console.log(numeriHuman)
 
-for(let i=0 ; i < numeriDado.length; i++){
-    console.log(numeriDado[i])
+let numeriBot = Math.floor(Math.random()*numeriDado.length)+1
+console.log(numeriBot)
+
+// creo le condizioni di vincita
+// se il tuo numero è più alto di quello del computer allora hai vinto tu
+// se i vostri numeri sono uguali allora avete pareggiato
+// se i numeri del pc sono più alti ha vinto lui
+
+if ( numeriHuman > numeriBot){
+    console.log('hai vinto tu!')
+    alert('hai vinto tu!')
+}else if ( numeriHuman == numeriBot){
+    console.log('pareggio!')
+    alert('Pareggio!')
+}
+else {
+    console.log('ha vinto il computer!')
+    alert('ha vinto il computer!')
 }
 
 
