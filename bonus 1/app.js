834d31e6be9ef5c09612a2bc20a75e2e490e2c8f
Bilobra <bra.carrozzo@gmail.com>
2022-07-26 10:49:50 +0200

    fix bonus 1

--- bonus 1/app.js
+++ bonus 1/app.js
@@ -3,13 +3,15 @@ console.log('prova bonus')
 let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 console.log(numberList)
+
+
 // senza ciclo con .slice 
-const Listnumber = numberList.slice(-5)
-console.log(Listnumber)
+// const Listnumber = numberList.slice(-5)
+// console.log(Listnumber)
 
 
-// non funziona
+
 // con ciclo for 
-// for (let i = numberList.length; i < numberList.length; i--){
-//     console.log(numberList[i])
-// }
+for (let i = (numberList.length - 1); i >= (numberList.length - 5); i--) {
+    console.log(numberList[i])
+}
